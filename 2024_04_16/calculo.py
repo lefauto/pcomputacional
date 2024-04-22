5f7ac6ed54bfda0fc58a371667630683668aa92d
lefauto <164242718+lefauto@users.noreply.github.com>
2024-04-22 08:19:20 -0300

    organizando

--- 2024_04_16/calculo.py
+++ 2024_04_16/calculo.py
@@ -1,8 +1,8 @@
 n1 = float(input("Insira o primeiro número:"))
 n2 = float(input("Insira o segundo número:"))
-area_r = n1*n2
+area_r = n1 * n2
 print("Área do retângulo:", area_r)
-peri_r = 2*(n1+n2)
+peri_r = 2 * (n1 + n2)
 print("Perímetro do retângulo:", peri_r)
-hip = (n1**2+n2**2)**(1/2)
+hip = (n1 ** 2 + n2 ** 2) ** (1 / 2)
 print("Hipotenusa:", hip)
