b1c029c77c661dfd93e6eae3254e0947f060d16d
lefauto <164242718+lefauto@users.noreply.github.com>
2024-04-30 12:45:41 -0300

    atualizando codigo entregavel 1

--- Entregável_1/nostring.py
+++ Entregável_1/nostring.py
@@ -1,47 +1,54 @@
 # Versão sem o conteúdo da aula de string
 
 # Definindo variáveis
-lista1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-char1 = '0'
-char2 = '0'
-char3 = '1'
-char0 = '1'
+listabase = [
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+     'x', 'y', 'z'],
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+     'X', 'Y', 'Z']
+    ]
+
 contador = 0
 
 
 # Alterando o valor do 1º caractere
-for valor1 in range(len(lista1)):
-    char1 = lista1[valor1]
+for w in range(len(listabase)):
+    for valor1 in range(len(listabase[w])):
+        char1 = listabase[w][valor1]
 
-    # Alterando o valor do 2º caractere
-    for valor2 in range(len(lista1)):
-        char2 = lista1[valor2]
+        # Alterando o valor do 2º caractere
+        for x in range(len(listabase)):
+            for valor2 in range(len(listabase[x])):
+                char2 = listabase[x][valor2]
 
-        # Alterando o valor do 3º caractere
-        for valor3 in range(len(lista1)):
-            char3 = lista1[valor3]
+                # Alterando o valor do 3º caractere
+                for y in range(len(listabase)):
+                    for valor3 in range(len(listabase[y])):
+                        char3 = listabase[y][valor3]
 
-            # Processo de filtração de caracteres repetidos
-            if char1 != char2 or char1 != char3:
-                passtry = char1 + char2 + char3
+                        # Processo de filtração de caracteres repetidos
+                        if char1 != char2 or char1 != char3:
+                            passtry = char1 + char2 + char3
 
-                # Escrevendo as possíveis senhas
-                print(passtry)
-                contador += 1
+                            # Escrevendo as possíveis senhas
+                            print(passtry)
+                            contador += 1
 
-            # Alterando o valor do 4º caractere
-            for valor0 in range(len(lista1)):
-                char0 = lista1[valor0]
+                        # Alterando o valor do 4º caractere
+                        for z in range(len(listabase)):
+                            for valor4 in range(len(listabase[z])):
+                                char4 = listabase[z][valor4]
 
-                # Processo de filtração de caracteres repetidos
-                if ((char1 != char2 and (char1 != char3 or char1 != char0) or
-                   (char1 != char3 and (char1 != char2 or char1 != char0)) or
-                   (char1 != char0 and (char1 != char3 or char1 != char2))) and
-                   (char2 != char3 or char2 != char0)):
+                                # Processo de filtração de caracteres repetidos
+                                if ((char1 != char2 and (char1 != char3 or char1 != char4) or
+                                     (char1 != char3 and (char1 != char2 or char1 != char4)) or
+                                     (char1 != char4 and (char1 != char3 or char1 != char2))) and
+                                   (char2 != char3 or char2 != char4)):
 
-                    # Escrevendo as possíveis senhas
-                    passtry = char1 + char2 + char3 + char0
-                    print(passtry)
-                    contador += 1
+                                    # Escrevendo as possíveis senhas
+                                    passtry = char1 + char2 + char3 + char4
+                                    print(passtry)
+                                    contador += 1
 
 print("Número de tentativas:", contador)
